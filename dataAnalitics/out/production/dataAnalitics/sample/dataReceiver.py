4131d19770b28b788a20fcb9bc37c24f9f342346
Trett Dragos <trettdragos@yahoo.com>
2018-05-30 21:45:30 +0300

    now knowing all connected user for notifications
    al users that receive notifications will be alerted in real time

--- dataAnalitics/out/production/dataAnalitics/sample/dataReceiver.py
+++ dataAnalitics/out/production/dataAnalitics/sample/dataReceiver.py
@@ -3,7 +3,7 @@ import time
 import os
 import sys
 
-ser = serial.Serial('COM8', 9600)
+ser = serial.Serial('/dev/ttyACM1', 9600)
 ser.close()
 ser.open()
 timeNow = str(time.strftime('%H-%M-%S-%d-%m-%Y'))+".txt"
@@ -11,14 +11,14 @@ timeNow = str(time.strftime('%H-%M-%S-%d-%m-%Y'))+".txt"
 #newFile = "" + str(timeNow)
 #a,b,c,d,e=newFile.split("'")
 newFile = os.path.join("db", timeNow)
-print "writing in file: "+newFile
+print ("writing in file:"+newFile)
 while 1:
-    temp = ser.readline()
-    #print temp
-    if (temp):
-        time, tempMotor, tempBatery, dump = temp.split(";")
-        print "Motor: "+tempMotor+ " - Batery: "+ tempBatery +" time: "+str(float(time)/10)+"s"
-        temp = temp[:-1]
-        fob = open(newFile, "a")
-        fob.write(temp)
-        fob.close()
+	temp = ser.readline()
+	#print temp
+	if (temp):
+		temp = temp[:-1]
+		time, tempMotor, tempBatery = temp.split(";")
+		print ("Motor: "+tempMotor+ " - Batery: "+ tempBatery +" time: "+str(float(time)/10)+"s")	
+		fob = open(newFile, "a")
+		fob.write(temp)
+		fob.close()
